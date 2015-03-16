bd4e659bb89f76bf31a0ccc4f28a68da4db7c957
yihho <F24011140@mail.ncku.edu.tw>
2015-03-16 17:08:38 +0800

    lab1

---
+++ lab1-B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n << " ";
+
+    while(n != 1)
+    {    
+        if (n % 2 == 1)
+        {
+            n = n * 3 + 1;
+            cout << n << " ";
+        }
+        else
+        {
+            n = n / 2;
+            cout << n << " ";
+        }
+    }
+    cout << endl;    
+    return 0;
+}
